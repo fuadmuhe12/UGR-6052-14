b1048b2fbf1baa37ce57c132d0ccae775d084d26
Fuad <fuad.mohamed@aait.edu.et>
2023-12-22 23:12:24 +0300

    finished the offline css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [".'Web individual portfolio assignment'/src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
