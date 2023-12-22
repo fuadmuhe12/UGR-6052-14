6597ba2b5c57fbdc1a53c9f08e431946db29a17b
Fuad <fuad.mohamed@aait.edu.et>
2023-12-22 13:06:57 +0300

    finished the home page

---
+++ Web individual portfolio assignment/src/JS/javas2.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const roles = [" Fullstack Developer", " Backend Developer", " UI/UX Designer"];
+  let roleIndex = 0;
+  let textIndex = 0;
+  let currentText = '';
+  let isDeleting = false;
+
+  function type() {
+    const speed = 100; // Adjust typing speed here
+    const targetElement = document.getElementById('typing');
+
+    if (!isDeleting && textIndex < roles[roleIndex].length) {
+      currentText += roles[roleIndex][textIndex];
+      textIndex++;
+    } else if (isDeleting && textIndex >= 0) {
+      currentText = roles[roleIndex].substring(0, textIndex);
+      textIndex--;
+    }
+
+    targetElement.innerHTML = currentText;
+
+    const typingSpeed = isDeleting ? speed / 2 : speed;
+
+    if (!isDeleting && textIndex === roles[roleIndex].length) {
+      isDeleting = true;
+      setTimeout(type, 1000); // Pause before deleting
+    } else if (isDeleting && textIndex === 0) {
+      isDeleting = false;
+      roleIndex = (roleIndex + 1) % roles.length;
+      setTimeout(type, 500); // Pause before typing the next role
+    } else {
+      setTimeout(type, typingSpeed);
+    }
+  }
+
+  type();
+});
